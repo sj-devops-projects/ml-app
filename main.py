d397fa68b468c70cf060aa60cdace483cb5e4579
szymonjakubiak <szymonjakubiak@gmail.com>
2024-05-04 12:43:50 +0000

    Use fastapi

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route("/")
-def index():
-    return render_template("index.html", post={"time": datetime.now()})
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html", context={"time": datetime.now()})
